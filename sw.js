458213ee245401d9876a0e4904e5d254db858058
dicostaempfli <dicostaempfli@gmail.com>
2026-06-12 20:20:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker — Raum für Selbstwirksamkeit
-// Version: 2026-06-12 20:15
+// Version: 2026-06-12 20:19
 
-const VERSION = '2026-06-12-2015';
+const VERSION = '2026-06-12-2019';
 
 // Bei Install: sofort aktivieren ohne auf alten SW zu warten
 self.addEventListener('install', e => {
